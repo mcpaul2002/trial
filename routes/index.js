92f6269dece188ad5d97cb52af996e648620d782
mc paul jr <msoriano@deakin.edu.au>
2020-10-07 08:53:57 +1100

    fixed files

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 //GET homepage.
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('index');
 });
 
 //GET rules
-router.get('/rules', function(req, res, next) {
+router.get('/rules', function(req, res) {
     res.render('rules');
 });
 
 //GET about
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
     res.render('about');
 });
 
 //GET private
-router.get('/private', function(req, res, next) {
+router.get('/private', function(req, res) {
     res.render('private');
 });
 
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express=require('express');
-const path = require('path');
-const http = require('http').createServer();
-const io = require('socket.io')(http);
-const indexRouter = require('./routes/index.js');
 
-let app=express();
+const indexRouter = require('./routes/index.js');
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+//socket.io connection
+io.on('connection', function (socket){
+  console.log('user connected');
+
+  //socket.io disconnection
+  socket.on('disconnect', function() {
+  console.log('user disconnected');
+});
+
+});
+
+
 
 //views engine setup
 app.set('view engine', 'hbs');
@@ -13,6 +26,8 @@ app.set('views', path.join(__dirname, 'views'));
 //serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 
 
@@ -20,7 +35,7 @@ app.use('/', indexRouter);
     
 
 // Start the server on port 3000
-app.listen(3000, function (){     
+http.listen(3000, function (){     
     console.log('Web server running at: http://localhost:3000');     
     console.log('Press Ctrl+C to shut down the web server'); 
   });
